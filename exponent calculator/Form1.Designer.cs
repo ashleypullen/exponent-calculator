2d3e64b1907bb36abb3a2f1853a95f5292a98489
AshleyPullen <85501589+ashleypullen@users.noreply.github.com>
2022-01-21 23:25:41 +0000

    initial commit

---
+++ exponent calculator/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace exponent_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxPower = new System.Windows.Forms.TextBox();
+            this.txtBoxNum = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(296, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Exponent Calculator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter Power";
+            // 
+            // txtBoxPower
+            // 
+            this.txtBoxPower.Location = new System.Drawing.Point(142, 131);
+            this.txtBoxPower.Name = "txtBoxPower";
+            this.txtBoxPower.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxPower.TabIndex = 3;
+            // 
+            // txtBoxNum
+            // 
+            this.txtBoxNum.Location = new System.Drawing.Point(142, 87);
+            this.txtBoxNum.Name = "txtBoxNum";
+            this.txtBoxNum.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxNum.TabIndex = 4;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(62, 183);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(180, 59);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 254);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtBoxNum);
+            this.Controls.Add(this.txtBoxPower);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxPower;
+        private System.Windows.Forms.TextBox txtBoxNum;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ exponent calculator/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exponent_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(txtBoxNum.Text);
+            int power = int.Parse(txtBoxPower.Text);
+
+            int sum = number;
+
+            for (int i = 0; i < (power - 1); i++)
+            {
+                sum = sum * number;
+            }
+            MessageBox.Show(sum.ToString());
+        }
+    }
+}
